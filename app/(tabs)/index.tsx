3d768bf8498063c02e91f8228c76f5bd3df9b37d
Ahmadov zaatreh <samorai-z@hotmail.com>
2026-05-16 19:52:09 +0300

    implement fixed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,11 +18,11 @@ export default function App() {
             <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary
             text-white p-4"> Go to Sign Up</Link>
 
-            <Link href="/app/subscriptions/spotify" >Spotify Subscription</Link>
+            <Link href="/subscriptions/spotify">Spotify Subscription</Link>
 
             <Link href={{
                 pathname: "/subscriptions/[id]",
-                params:{id: "claude"}
+                params:{id: "Claude"}
             }} >Claude max Subscription</Link>
 
 
